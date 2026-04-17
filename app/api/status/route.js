93c6788ef1a065f428985b330cabbdae2e6a54c2
sayand-raj <sayandsr@gmail.com>
2026-04-17 19:43:44 +0530

    schoolmanagement

--- app/api/status/route.js
+++ app/api/status/route.js
@@ -1,4 +1,3 @@
-// app/api/dashboard/route.js
 import { connectDB } from "@/lib/db";
 import Student from "../../models/Student";
 import Teacher from "../../models/Teacher";
---
+++ app/attendence/page.js
@@ -0,0 +1,12 @@
+
+"use client";
+
+import React from "react";
+
+export default function AttendencePage() {
+  return (
+    <div>
+      <h1>Attendence Page</h1>
+    </div>
+  );
+}
